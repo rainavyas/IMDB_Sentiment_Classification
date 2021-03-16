24e126d1ecced895b2f32714fe39c15a5c549327
Vyas Raina <sayvaniar@gmail.com>
2021-03-16 15:58:47 +0000

    citation

--- data_prep.py
+++ data_prep.py
@@ -1,10 +1,34 @@
 '''
 Prepare the IMDB training data as tokenized pytorch
-id tensor and attention mask
+ids tensor and attention mask
 '''
 import torch
 import scandir
 from transformers import ElectraTokenizer
+
+_DESCRIPTION = """\
+Large Movie Review Dataset.
+This is a dataset for binary sentiment classification containing substantially \
+more data than previous benchmark datasets. We provide a set of 25,000 highly \
+polar movie reviews for training, and 25,000 for testing. There is additional \
+unlabeled data for use as well.\
+"""
+
+_CITATION = """\
+@InProceedings{maas-EtAl:2011:ACL-HLT2011,
+  author    = {Maas, Andrew L.  and  Daly, Raymond E.  and  Pham, Peter T.  and  Huang, Dan  and  Ng, Andrew Y.  and  Potts, Christopher},
+  title     = {Learning Word Vectors for Sentiment Analysis},
+  booktitle = {Proceedings of the 49th Annual Meeting of the Association for Computational Linguistics: Human Language Technologies},
+  month     = {June},
+  year      = {2011},
+  address   = {Portland, Oregon, USA},
+  publisher = {Association for Computational Linguistics},
+  pages     = {142--150},
+  url       = {http://www.aclweb.org/anthology/P11-1015}
+}
+"""
+
+_DOWNLOAD_URL = "http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
 
 def get_reviews(dir):
     review_files = [f.name for f in scandir.scandir(dir)]
