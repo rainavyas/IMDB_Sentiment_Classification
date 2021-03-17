418b35128154640060b2658823cecb861cdf68bb
Vyas Raina <sayvaniar@gmail.com>
2021-03-17 13:16:46 +0000

    default params

--- train.py
+++ train.py
@@ -87,8 +87,8 @@ if __name__ == "__main__":
     commandLineParser = argparse.ArgumentParser()
     commandLineParser.add_argument('OUT', type=str, help='Specify output th file')
     commandLineParser.add_argument('--B', type=int, default=16, help="Specify batch size")
-    commandLineParser.add_argument('--epochs', type=int, default=25, help="Specify epochs")
-    commandLineParser.add_argument('--lr', type=float, default=0.001, help="Specify learning rate")
+    commandLineParser.add_argument('--epochs', type=int, default=2, help="Specify epochs")
+    commandLineParser.add_argument('--lr', type=float, default=0.000001, help="Specify learning rate")
     commandLineParser.add_argument('--sch', type=int, default=10, help="Specify scheduler rate")
     commandLineParser.add_argument('--seed', type=int, default=1, help="Specify seed")
 
